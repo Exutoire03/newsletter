20b976cdecdad12ddf2d94de1036db37588324ab
Exutoire03 <cheikh.copywriter@gmail.com>
2025-01-13 23:44:00 +0100

    init project

---
+++ app/api/subscribe/route.ts
@@ -0,0 +1,38 @@
+import mailchimp from "@mailchimp/mailchimp_marketing"
+import { subscribe } from "diagnostics_channel"
+import { NextResponse } from "next/server"
+
+mailchimp.setConfig({
+    apiKey: process.env.MAILCHIMP_API_KEY!,
+    server : process.env.MAILCHIMP_API_SERVER!
+})
+
+export async function POST(request: Request) {
+    try {
+        
+        const {email} = await request.json()
+
+        if(!email){
+            return NextResponse.json(
+                {error: "L'adresse mail est recquise !"},
+                { status: 400 }
+            )
+        }
+
+        const res = await mailchimp.lists.addListMember(
+            process.env.MAILCHIMP_AUDIENCE_ID!,
+            {email_address: email, status: "subscribed"}
+        )
+
+        return NextResponse.json(
+            {message: "Cette adresse e-mail a été inscrite avec succès !", data : res},
+            { status: 201 }
+        )
+
+    } catch (error) {
+        return NextResponse.json(
+            {error: "Cette adresse e-mail est déjà inscrite !"},
+            { status: 500 }
+        )
+    }
+}
